31204b9f7d00d3e0d28e83ad3e94f2fec5702d15
Gabriel Vicente <gabrielusvicente@gmail.com>
2015-06-10 23:11:07 -0300

    Initial Commit

---
+++ Euclidean/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Euclidean
+//
+//  Created by Gabriel Vicente on 6/10/15.
+//  Copyright (c) 2015 Gabriel Vicente. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
