daf67bd922afeee0cb4b5ff505f390ea9a3ee6c0
g4brielvs <gabrielusvicente@gmail.com>
2015-06-10 23:45:02 -0300

    Add Euclidean algorithm

--- Euclidean/main.c
+++ Euclidean/main.c
@@ -7,9 +7,36 @@
 //
 
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+ 
+int gcd(int a, int b);
+ 
+int main(void)
+{
+    int a, b;
+     
+    scanf("%d %d", &a, &b);
+     
+    printf("The iterative GDC is %d\n", iterative_gcd(a, b));
+    printf("The recursive GCD is %d\n", recursive_gcd(a, b));
+ 
     return 0;
+}
+ 
+ // Iterative version:
+ int iterative_gcd(int a, int b) {
+    int t;
+    while (b != 0) {
+        t = b;
+        b = a % b;
+        a = t;
+    }
+    return a;
+}
+
+// Recursive version:
+int recursive_gcd(int a, int b) {
+    if (b == 0)
+        return a;
+     
+    return gcd(b, a % b);
 }
